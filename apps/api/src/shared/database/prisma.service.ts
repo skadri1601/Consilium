4e8759ee052e2220e654945f4dda42af3f6a2a3a
Your Name <mxk3702@mavs.uta.edu>
2026-02-01 23:54:32 -0600

    db chg

--- apps/api/src/shared/database/prisma.service.ts
+++ apps/api/src/shared/database/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy, Logger } from "@nestjs/common";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@consilium/database";
 
 @Injectable()
 export class PrismaService
